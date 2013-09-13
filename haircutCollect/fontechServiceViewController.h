a527e8bc9a47a9b6a9bbc1b837ff0ae6c80fe792
icelorc <icelorc@gmail.com>
2013-09-13 11:17:25 +0800

    add view controllers to the project

---
+++ haircutCollect/fontechServiceViewController.h
@@ -0,0 +1,13 @@
+//
+//  fontechServiceViewController.h
+//  haircutCollect
+//
+//  Created by Robert Huang on 9/13/13.
+//  Copyright (c) 2013 Robert Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fontechServiceViewController : UITableViewController
+
+@end
