cc8d1a300a999635a839501f904806ffddb4bb03
icelorc <icelorc@gmail.com>
2013-09-24 13:20:00 +0800

    add new facebook page to the project

--- haircutCollect/fontechFacebookViewController.m
+++ haircutCollect/fontechFacebookViewController.m
@@ -1,18 +1,18 @@
 //
-//  fontechSearchViewController.m
+//  fontechFacebookViewController.m
 //  haircutCollect
 //
 //  Created by Robert Huang on 9/24/13.
 //  Copyright (c) 2013 Robert Huang. All rights reserved.
 //
 
-#import "fontechSearchViewController.h"
+#import "fontechFacebookViewController.h"
 
-@interface fontechSearchViewController ()
+@interface fontechFacebookViewController ()
 
 @end
 
-@implementation fontechSearchViewController
+@implementation fontechFacebookViewController
 
 @synthesize webView = _webView;
 
@@ -28,10 +28,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSString *urlAddress = @"https://www.google.com.tw/?gws_rd=cr&ei=JQ1BUo6SBcqukgXfpoHgDQ#q=Flux+Collection+Hair";
+    NSString *urlAddress = @"http://m.facebook.com/pages/FLUX_collection-HAIR-BOUTIQUE/151421662525?ref=br_tf&_rdr";
     NSURL *url = [NSURL URLWithString:urlAddress];
-    NSURLRequest *requestObj1 = [NSURLRequest requestWithURL:url];
-    [self.webView loadRequest:requestObj1];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:requestObj];
+
 }
 
 - (void)didReceiveMemoryWarning
