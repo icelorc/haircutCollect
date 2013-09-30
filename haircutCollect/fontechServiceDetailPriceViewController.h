d8b814ce64ff884061990d2682a0abe2c13df638
icelorc <icelorc@gmail.com>
2013-09-30 14:46:26 +0800

    add detail service price view

---
+++ haircutCollect/fontechServiceDetailPriceViewController.h
@@ -0,0 +1,13 @@
+//
+//  fontechServiceDetailPriceViewController.h
+//  haircutCollect
+//
+//  Created by Robert Huang on 9/30/13.
+//  Copyright (c) 2013 Robert Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fontechServiceDetailPriceViewController : UITableViewController
+
+@end
